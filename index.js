674335a3ca19b3467fe86db050ffc49453ea03b2
llupRisinglll <luisedward.miranda@gmail.com>
2022-11-30 16:50:01 +0800

    chore: finish up setup of mongodb

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+// load dotenv
+require('dotenv').config()
 
 
 app.get('/', (req, res) => {
+
+	const Cat = mongoose.model('Cat', {
+		name: String 
+	});
+
+	const kitty = new Cat({ name: 'Zildjian' });
+	kitty.save().then(() => console.log('meow'));
+
 	res.send('Hello World!')
 })
 
-app.listen(port, () => {
 
+const mongoose = require('mongoose');
+
+// connect to the database
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+app.listen(port, () => {
 
 	console.log(`Example app listening on port ${port}`)
 })
