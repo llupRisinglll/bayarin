e5643ec8657f839f892896e19b4aba102289bffa
llupRisinglll <luisedward.miranda@gmail.com>
2022-11-30 16:42:18 +0800

    chore: added dotenv

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 
 app.get('/', (req, res) => {
